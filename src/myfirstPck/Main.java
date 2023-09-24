3da1d665e57e950ac2c93c043c3b53c967c48d24
Kateryna Fedchenko <katya210883@gmail.com>
2023-09-24 16:20:11 +0200

    my first commit

---
+++ src/myfirstPck/Main.java
@@ -0,0 +1,19 @@
+package myfirstPck;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Kyiv");
+        float celsius, kelvin, fahrenheit;
+        celsius = 25.0f;
+        fahrenheit = (9.0f * celsius + 5.0f * 32.0f) / 5.0f;
+        kelvin = celsius + 273.16f;
+        System.out.println(celsius + " degrees Celsius: "
+                + fahrenheit + " Fahrenheit, "
+                + kelvin + " Kelvin");
+        float leg1, leg2, areaTriangle;
+        leg1 = 5.0f;
+        leg2 = 3.0f;
+        areaTriangle = (leg1*leg2)/2.0f;
+        System.out.println("Area of a Triangle: " + areaTriangle + " square meters");
+    }
+}
